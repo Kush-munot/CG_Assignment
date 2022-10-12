f0005a67981949c243ce7f78118ed0c786175315
Kush-munot <munot.kgm@gmail.com>
2022-10-12 23:50:01 +0530

    Added Image to text Converter

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
 import cv2;
-image = cv2.imread("./assets/demo.jpg")
+image = cv2.imread("./assets/demo2.png")
 
 grey_filter = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-cv2.imwrite("graysacle.jpg",grey_filter)
+cv2.imwrite("graysacle.png",grey_filter)
 
 invert = cv2.bitwise_not(grey_filter)
-cv2.imwrite("invert.jpg",invert)
+cv2.imwrite("invert.png",invert)
 
 blur = cv2.GaussianBlur(invert,(21,21),0)
-cv2.imwrite("blur.jpg",blur)
+cv2.imwrite("blur.png",blur)
 
 invertedBlur = cv2.bitwise_not(blur)
-cv2.imwrite("invBlur.jpg",invertedBlur)
+cv2.imwrite("invBlur.png",invertedBlur)
 
 sketch = cv2.divide(grey_filter,invertedBlur,scale=256.0)
-cv2.imwrite("sketch.jpg",sketch)
+cv2.imwrite("sketch.png",sketch)
