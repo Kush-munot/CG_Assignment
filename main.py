4a8501113d634b7791688fec26574b5807661c18
Pratham2301 <prathamrajbhoj2003@gmail.com>
2022-10-16 22:23:11 +0530

    adding switch case

---
+++ main.py
@@ -0,0 +1,27 @@
+print("Computer Graphics TA 1 ---- Group 1")
+
+while(1):
+    print("Choices Available are : ")
+    print("0.   Exit")
+    print("1.   Image to Sketch")
+    print("2.   Image to Cartoon")
+    print("3.   Image to Filter")
+    print("\n\n\n")
+    opt = int(input("Enter Your Choice "))
+    match opt:
+        case 0:
+            break
+
+        case 1:
+            print("1")
+
+        case 2:
+            print("2")
+        
+        case 3:
+            print("3")
+
+        case 4:
+            print("4")
+        case _:
+            print("default")
